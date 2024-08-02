69d5ac8dcb793be452dba0c75dd8bdb247589c35
Dhritiman Saha <dhritiman4th@gmail.com>
2024-08-02 22:13:35 +0530

    feat: Add change password, current user, and update account details endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,14 @@
 import {Router} from "express";
-import {loginUser, logoutUser, registerUser, refreshAccessToken, updateUserAvatar} from "../controllers/user.controller.js";
+import {
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    updateUserAvatar, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails
+} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
@@ -32,10 +41,40 @@ router.route("/refresh-token").post(
     refreshAccessToken
 );
 
-router.route("/update-avatar").post(
+router.route("/change-password").post(
+    verifyJWT,
+    changeCurrentPassword
+);
+
+router.route("/current-user").get(
+    verifyJWT,
+    getCurrentUser
+);
+
+router.route("/update-account-details").patch(
+    verifyJWT,
+    updateAccountDetails
+);
+
+router.route("/update-avatar").patch(
     verifyJWT,
     upload.single("avatar"),
     updateUserAvatar
 );
 
+router.route("/update-cover").patch(
+    verifyJWT,
+    upload.single("coverImage"),
+    updatedCoverImage
+);
+
+router.route("/channel-profile/:username").get(
+    verifyJWT,
+    getUserChannelProfile
+);
+
+route.route("/watch-history").get(
+    verifyJWT, 
+    getWatchHistory
+);
 export default router;
